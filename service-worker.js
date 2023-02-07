03de9fbe5f5c3348d9b1180b1080808d4d74c341
Rehab Mulla <rehab.mulla@gmail.com>
2023-02-07 10:24:55 +0400

    hi

--- service-worker.js
+++ service-worker.js
@@ -14,20 +14,32 @@ var casheFiles = [
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
     e.waitUntil(
-        caches.open(casheName).then((cashe) => {
+        caches.open(cacheName).then((cache) => {
             console.log('[Service Worker] Caching all the files');
-            return cashe.addAll(casheFiles);
+            return cache.addAll(cacheFiles);
         })
     );
 
 })
 
+//self.addEventListener('fetch', function (e){
+    //e.respondWith(
+       // caches.match(e.request).then(function (r) {
+       //     console.log('[Service Worker] Fetching resource:' 
+       //     + e.request.url);
+       //     return r
+    //    })
+  //  );
+//});
 self.addEventListener('fetch', function (e){
     e.respondWith(
         caches.match(e.request).then(function (r) {
-            console.log('[Service Worker] Fetching resource:' 
-            + e.request.url);
-            return r
-        })
-    );
+           return r || fetch(e.request).then(function(response){
+            return caches.open(cacheName).then(function (cache){
+                cache.put(e.request, response.clone());
+                return response;
+            });
+           });
+       })
+   );
 });
